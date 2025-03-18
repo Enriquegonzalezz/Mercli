798999d980951798d47793f7c942b14cc46e98a3
Samuel <samuelmolina664@gmail.com>
2025-03-17 23:13:33 -0400

    Union of docker-compose.yml

--- User Service/db/init/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS prueba;
-USE prueba;
-
-CREATE TABLE IF NOT EXISTS user(
-	id int auto_increment primary key,
-    username varchar(500),
-    email varchar(500),
-    password varchar(500)
-);
